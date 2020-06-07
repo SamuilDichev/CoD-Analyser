3d02938a367d395804c2d4fc586ca88e897bc596
Samuil Dichev <samuild@mustardsystems.com>
2020-06-07 12:06:48 +0100

    Add KDR and SPM distribution graphs

--- src/match.py
+++ src/match.py
@@ -7,7 +7,8 @@ import requests
 
 from src.exception import CoDTrackerApiException
 from src.player import Player
-
+import numpy as np
+import matplotlib.pyplot as plt
 
 class Match:
 	def __init__(self, match_id):
@@ -20,6 +21,7 @@ class Match:
 			raise CoDTrackerApiException(response.text)
 
 		data = json.loads(response.text)
+		self.timestamp = data["data"]["metadata"]["timestamp"]
 		segments = data["data"]["segments"]
 		executor = concurrent.futures.ThreadPoolExecutor(20)
 		futures = [executor.submit(self.scrape_player, segment) for segment in segments]
@@ -35,12 +37,11 @@ class Match:
 
 
 def output_results(match: Match):
-	with open(match.match_id + ".csv", "w") as csv_file:
+	kdr_list = []
+	spm_list = []
+	with open(match.timestamp + "_" + match.match_id + ".csv", "w") as csv_file:
 		writer = csv.DictWriter(csv_file, fieldnames=["username", "platform", "K/D", "Score Per Minute"])
 		writer.writeheader()
-
-		kdr_list = []
-		spm_list = []
 		pc_players = 0
 		console_players = 0
 		for player in match.players:
@@ -65,6 +66,28 @@ def output_results(match: Match):
 			"Score Per Minute": sum(spm_list) / len(spm_list)
 		})
 
+		generate_distribution_graphs(match, kdr_list, spm_list)
+
+
+def generate_distribution_graphs(match: Match, kdr_list, spm_list):
+	fig, axs = plt.subplots(1, 2)
+	fig.tight_layout()
+	# 1920x540 (Half height of 1080p)
+	fig.set_size_inches(19.2, 5.4)
+
+	axs[0].set_title('K/D distribution for match ' + match.match_id)
+	axs[0].set(xlabel='K/D', ylabel='# of players')
+	axs[0].set_xlim(0, 5.1)
+	axs[0].set_xticks([tick/10 for tick in range(5, 51, 5)])
+	axs[0].hist(kdr_list, np.arange(0, 5, 0.1), alpha=0.5, histtype='bar', ec='black')
+
+	axs[1].set_title('Score/min distribution for match ' + match.match_id)
+	axs[1].set(xlabel='Score/min', ylabel='# of players')
+	axs[1].set_xlim(0, 510)
+	axs[1].set_xticks([tick*10 for tick in range(5, 51, 5)])
+	axs[1].hist(spm_list, np.arange(0, 500, 10), alpha=0.5, histtype='bar', ec='black')
+
+	plt.savefig(match.timestamp + "_" + match.match_id + ".png")
 
 def main():
 	match_id = input("Enter a match ID (from cod.tracker.gg): ")
